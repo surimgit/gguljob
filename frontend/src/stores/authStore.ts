601773006dc75199de47b459c5e7c7609b3e882a
이도현 <ehtm01@gmail.com>
2026-03-09 16:50:25 +0900

    [S14P21E107-185] feature : Zustand 상태관리 초기 설정

---
+++ frontend/src/stores/authStore.ts
@@ -0,0 +1,19 @@
+import { create } from 'zustand';
+import type { User } from '../types/user';
+
+interface AuthState {
+  user: User | null;
+  isAuthenticated: boolean;
+  setUser: (user: User) => void;
+  logout: () => void;
+}
+
+export const useAuthStore = create<AuthState>((set) => ({
+  user: null,
+  isAuthenticated: false,
+  setUser: (user) => set({ user, isAuthenticated: true }),
+  logout: () => {
+    localStorage.removeItem('accessToken');
+    set({ user: null, isAuthenticated: false });
+  },
+}));
