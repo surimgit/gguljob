3c616c29e9a699837cad66edc91a0f901ea4d92d
김민종 <kmj010105@naver.com>
2026-03-09 17:06:05 +0900

    [S14P21E107-176] feature: Swagger UI 적용 및 전역 예외 처리(GlobalException) 세팅

---
+++ backend/src/main/java/com/ssafy/project/backend/global/api/ErrorCode.java
@@ -0,0 +1,9 @@
+package com.ssafy.project.backend.global.api;
+
+import org.springframework.http.HttpStatus;
+
+public interface ErrorCode {
+    HttpStatus status();
+    String defaultMessage();
+    String code();
+}
