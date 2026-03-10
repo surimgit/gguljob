d10fb3f2bf12d74b4d74ff19fa8efe4160a39240
김민종 <kmj010105@naver.com>
2026-03-10 12:41:09 +0900

    [S14P21E107-59] fix: UserRepository.java 주석 수정

--- backend/src/main/java/com/ssafy/gguljob/backend/domain/user/repository/UserRepository.java
+++ backend/src/main/java/com/ssafy/gguljob/backend/domain/user/repository/UserRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    // 이메일로 가입된 유저가 있는지 찾rl
+    // 이메일로 가입된 유저가 있는지 찾기
     Optional<User> findByEmail(String email);
 
     String email(String email);
