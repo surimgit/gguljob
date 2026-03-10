da163b93c7a585fe6450f2005194e3447df0541d
이도현 <ehtm01@gmail.com>
2026-03-10 16:51:27 +0900

    [S14P21E107-68] chore: 테스트 버튼 삭제

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,20 +1,4 @@
-import { useState } from 'react';
-import ProfileSetupModal from '../components/feature/auth/ProfileSetupModal';
-
 const Home = () => {
-  // TODO: 테스트용 임시 코드 - 테스트 완료 후 제거
-  const [showModal, setShowModal] = useState(false);
-
-  return (
-    <>
-      <h1>메인 페이지입니다.</h1>
-      <button onClick={() => setShowModal(true)}>프로필 모달 테스트</button>
-      <ProfileSetupModal
-        isOpen={showModal}
-        onClose={() => setShowModal(false)}
-        onComplete={(data) => console.log('완료:', data)}
-      />
-    </>
-  );
+  return <h1>메인 페이지입니다.</h1>;
 };
 export default Home;
