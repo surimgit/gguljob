69809d1514615a1ab2725ef8d9c4425e72633962
김민종 <kmj010105@naver.com>
2026-03-20 09:56:12 +0900

    fix: 초대 수락/거절 api 수정

--- backend/src/main/java/com/ssafy/gguljob/backend/domain/join/event/JoinRequestEvent.java
+++ backend/src/main/java/com/ssafy/gguljob/backend/domain/join/event/JoinRequestEvent.java
@@ -7,23 +7,26 @@ import lombok.Getter;
 public class JoinRequestEvent {
     private final Long targetUserId; // 알림을 받을 사람 (지원자 or 프로젝트 리더)
     private final Long projectId;    // 관련된 프로젝트 ID
+    private final Long requestId;    // 초대장 ID
     private final String message;    // 알림 내용
     private final String category;   // 알림 카테고리 (예: "JOIN_ACCEPT", "JOIN_REJECT")
 
     private final LocalDateTime createdAt;
 
-    public JoinRequestEvent(Long targetUserId, Long projectId, String message, String category) {
+    public JoinRequestEvent(Long targetUserId, Long projectId, Long requestId, String message, String category) {
         this.targetUserId = targetUserId;
         this.projectId = projectId;
+        this.requestId = requestId;
         this.message = message;
         this.category = category;
         this.createdAt = LocalDateTime.now();
     }
 
     // 테스트 및 외부 주입을 고려한 오버로딩 생성자
-    public JoinRequestEvent(Long targetUserId, Long projectId, String message, String category, LocalDateTime createdAt) {
+    public JoinRequestEvent(Long targetUserId, Long projectId, Long requestId, String message, String category, LocalDateTime createdAt) {
         this.targetUserId = targetUserId;
         this.projectId = projectId;
+        this.requestId = requestId;
         this.message = message;
         this.category = category;
         this.createdAt = createdAt;
